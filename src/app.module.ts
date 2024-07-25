a4d0b5161f26862145724a436b9dcf63af04d325
Diego Arenas -Dev <diegoarenas111@gmail.com>
2024-07-24 22:39:15 -0500

    adding validation with joi to ENV
    add swagger documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
+import * as Joi from 'joi';
+
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { enviroments } from './enviroments';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import config from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
     ProductsModule,
     UsersModule,
     DatabaseModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
