7f88db50f164ce62fb5ebd5031de668b0f585388
Diego Arenas -Dev <diegoarenas111@gmail.com>
2024-08-02 21:57:09 -0500

    was added the end points and services to create, update, and delete the products

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -1,13 +1,18 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
   ParseIntPipe,
+  Post,
+  Put,
 } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ProductsService } from 'src/products/services/products.service';
+import { CreateProductDto, UpdateProdcutDto } from '../dtos/products.dto';
 
 //* A difrencia de app controller nuestro decorador contiene @controller('products')
 @ApiTags('products')
@@ -20,10 +25,6 @@ export class ProductsController {
   @Get('')
   @HttpCode(HttpStatus.ACCEPTED)
   getProducts() {
-    // @Query('brand') brand: string, // @Query('offset') offset: number = 0, // @Query('limit') limit: number = 100,
-    // return {
-    //   message: `products con limit:${limit} y offset:${offset} y la brand: ${brand}`,
-    // };
     return this.productsService.findAll();
   }
 
@@ -40,36 +41,24 @@ export class ProductsController {
     description: 'you would send a Id in the url, then returns the product',
   })
   getProductById(@Param('productId', ParseIntPipe) productId: number) {
-    // return `product id: ${productId}`;
     return this.productsService.findOne(productId);
   }
 
-  // @Post()
-  // create(@Body() payload: CreateProductDto) {
-  //   // return {
-  //   //   message: 'producto creado',
-  //   //   payload,
-  //   // };
-  //   return this.productsService.create(payload);
-  // }
+  @Post()
+  create(@Body() payload: CreateProductDto) {
+    return this.productsService.create(payload);
+  }
 
-  // @Put(':id')
-  // update(
-  //   @Param('id', ParseIntPipe) id: number,
-  //   @Body() payload: UpdateProdcutDto,
-  // ) {
-  //   // return {
-  //   //   id,
-  //   //   payload,
-  //   // };
-  //   return this.productsService.update(id, payload);
-  // }
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateProdcutDto,
+  ) {
+    return this.productsService.update(id, payload);
+  }
 
-  // @Delete(':id')
-  // delete(@Param('id', ParseIntPipe) id: number) {
-  //   // return {
-  //   //   message: `the element with the id:${id} was deleted`,
-  //   // };
-  //   return this.productsService.delete(id);
-  // }
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.delete(id);
+  }
 }
--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-// import {
-//   CreateProductDto,
-//   UpdateProdcutDto,
-// } from 'src/products/dtos/products.dto';
+import {
+  CreateProductDto,
+  UpdateProdcutDto,
+} from 'src/products/dtos/products.dto';
 import { Product } from 'src/products/entities/product.entity';
 import { Repository } from 'typeorm';
 
@@ -12,18 +12,6 @@ export class ProductsService {
   constructor(
     @InjectRepository(Product) private productRepo: Repository<Product>,
   ) {}
-
-  // private counterId = 1;
-  // private products: Product[] = [
-  //   {
-  //     id: 1,
-  //     name: 'product1',
-  //     description: 'bla bla',
-  //     price: 122,
-  //     image: '',
-  //     stock: 2,
-  //   },
-  // ];
 
   findAll() {
     return this.productRepo.find();
@@ -37,39 +25,25 @@ export class ProductsService {
     return product;
   }
 
-  // private findIndex(id: Product['id']) {
-  //   const index = this.products.findIndex((item) => item.id === id);
-  //   if (index === -1) throw new NotFoundException(`Product with the id #${id}`);
-  //   return index;
-  // }
+  create(payload: CreateProductDto): Promise<Product> {
+    // const newProduct = new Product();
+    // newProduct.image = payload.image;
+    // newProduct.name = payload.name;
+    // newProduct.description = payload.description;
+    // newProduct.price = payload.price;
+    // newProduct.stock = payload.stock;
+    const newProduct = this.productRepo.create(payload);
 
-  // create(payload: CreateProductDto): Product {
-  //   this.counterId += 1;
-  //   const newProduct = {
-  //     id: this.counterId,
-  //     ...payload,
-  //   };
-  //   this.products.push(newProduct);
-  //   return newProduct;
-  // }
+    return this.productRepo.save(newProduct);
+  }
 
-  // update(id: Product['id'], data: UpdateProdcutDto): Product {
-  //   const productIndex = this.findIndex(id);
-  //   const product = this.products[productIndex];
+  async update(id: Product['id'], data: UpdateProdcutDto): Promise<Product> {
+    const product = await this.findOne(id);
+    this.productRepo.merge(product, data);
+    return this.productRepo.save(product);
+  }
 
-  //   const updatedProduct: Product = {
-  //     ...product,
-  //     ...data,
-  //   };
-
-  //   this.products[productIndex] = updatedProduct;
-
-  //   return updatedProduct;
-  // }
-
-  // delete(id: Product['id']) {
-  //   const productIndex = this.findIndex(id);
-  //   const deletedProduct = this.products.splice(productIndex, 1)[0];
-  //   return deletedProduct;
-  // }
+  delete(id: Product['id']) {
+    return this.productRepo.delete(id);
+  }
 }
